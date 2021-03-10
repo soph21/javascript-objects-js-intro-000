adbddcf04f79dfdfa67fa00eec184e96eca467d8
Sophie Shin <sophie@alloy.co>
2021-03-10 15:04:10 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,8 @@ function updatePlaylist (playlist, artistName,songTitle) {
 playlist['Phil Ochs']= 'Slowdive'
   return playlist
 }
-function removeFromPlaylist(playlist) {
+function removeFromPlaylist(playlist, artistName,songTitle) {
+playlist['Phil Ochs'] = 'Slowdive'
   delete playlist['Phil Ochs']
   return playlist['My Bloody Valentine']= 'When you Sleep'
 }
